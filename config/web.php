7eaadee9c86e6567776e3c221fb91f69d4a0cfa7
jhaturaphat <umt.eng52@gmail.com>
2022-10-18 11:04:08 +0700

    edit databases

--- config/web.php
+++ config/web.php
@@ -2,7 +2,7 @@
 
 $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
-$hosxp = require __DIR__ . '/hosxp.php';
+
 
 $config = [
     'id' => 'basic',
@@ -85,8 +85,7 @@ $config = [
                 ],
             ],
         ],
-        'db' => $db,
-        'hosxp' => $hosxp,
+        'db' => $db,        
         /*
         'urlManager' => [
             'enablePrettyUrl' => true,
